7688357b5ace8442957d260fe52c15b300b4e0b9
ErikaMiklos <perkamma@gmail.com>
2022-11-28 21:22:32 +0100

    friendly-chat firebase nodejs

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -8,6 +8,12 @@
  */
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyB3m4zb4kCsW9GSB6JhqHd7Dmavx9egTP4",
+  authDomain: "friendlychat-a73a5.firebaseapp.com",
+  projectId: "friendlychat-a73a5",
+  storageBucket: "friendlychat-a73a5.appspot.com",
+  messagingSenderId: "145032357338",
+  appId: "1:145032357338:web:137ac61711f52bd76e8ce3"
 };
 
 export function getFirebaseConfig() {
